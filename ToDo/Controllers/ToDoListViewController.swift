7670af7ae2e311f2df8dc98675f1e0de9d262a25
elaniacs <carensousaesilva@gmail.com>
2022-12-16 08:46:36 -0300

    Update and delete data with Core Data

--- ToDo/Controllers/ToDoListViewController.swift
+++ ToDo/Controllers/ToDoListViewController.swift
@@ -47,7 +47,9 @@ class ToDoListViewController: UITableViewController {
     
     //detects which line was selected
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        print(itemArray[indexPath.row])
+        
+        context.delete(itemArray[indexPath.row])
+        itemArray.remove(at: indexPath.row)
         
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
